8d92926afb05d9b8fa204d74246a7ff1edec5ceb
Ulises <usanchez@gmail.com>
2018-07-13 13:36:11 +0200

    localdb

--- DataAccessLayer/Classes/BJDBContext.cs
+++ DataAccessLayer/Classes/BJDBContext.cs
@@ -1,11 +1,12 @@
-﻿using System.Data.Entity;
+﻿using System.Configuration;
+using System.Data.Entity;
 using GameCardLib;
 
 namespace DAL
 {
     public class BJDBContext : DbContext
     {
-        public BJDBContext() : base("LocalDB") { }
+        public BJDBContext() : base(ConfigurationManager.ConnectionStrings["LocalDB"].ConnectionString) { }
         public DbSet<Game> Games { get; set; }
         public DbSet<Player> Players { get; set; }
         public DbSet<Deck> Decks { get; set; }
